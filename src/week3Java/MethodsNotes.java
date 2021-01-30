4d6508b2e51759d61e24ca0510da7a1480da5dfa
Brandelyn Duenckel <brandelyns@gmail.com>
2021-01-29 17:40:33 -0700

    added first commit

--- src/week3Java/MethodsNotes.java
+++ src/week3Java/MethodsNotes.java
@@ -3,8 +3,18 @@ package week3Java;
 public class MethodsNotes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String firstName = "Bob";
+		String lastName = "Ross";
+		String fullName = firstName + " " + lastName;
+		
+		System.out.println(fullName);
+		
+		
+	}
+	
+	public static String createFullName(String firstName, String lastName) {//3:17
+		
 	}
 
 }
